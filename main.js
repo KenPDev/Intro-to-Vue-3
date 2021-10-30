c18fce184353ee0f00b8999a3a36875c211882fb
Ken Pink <kenpink@gmail.com>
2021-10-30 11:08:08 -0400

    L5 completed

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
+            image: './assets/images/socks_blue.jpg',
+            inStock: true,
+            details: ['50% Cotton', '30% Wool', '100% Hemp'],
+            variants: [
+                {id: 2234, color: 'green'},
+                {id: 2235, color: 'blue'}
+            ],
+            sizes: ['big', 'small', 'just medium']
+            
         }
     }
 })
